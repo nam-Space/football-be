22dbd1539afeee64b4feca123e7887c48b30e74e
PTITer2003 <namd21ptit@gmail.com>
2025-03-14 18:22:23 +0700

    add api

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const { loginUser, createUser, getUserAccount, logoutUser } = require('../controllers/userController');
+const { loginUser, createUser, getUserAccount, logoutUser, getAllUsers, updateUser, deleteUser } = require('../controllers/userController');
 
 const router = express.Router()
 
+router.get('/', getAllUsers)
 router.post('/login', loginUser)
 router.post('/register', createUser)
-router.get('/account', getUserAccount)
 router.get('/logout', logoutUser)
+router.get('/account', getUserAccount)
+router.post('/create', createUser)
+router.post('/update', updateUser)
+router.delete('/delete/:id', deleteUser)
 
 module.exports = router;
