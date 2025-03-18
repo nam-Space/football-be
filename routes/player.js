bfa2688d6071e9fa105e7e2106291168986b0937
TraMi2k3 <MiNTT.B21CN518@stu.ptit.edu.vn>
2025-03-18 15:34:49 +0700

    Update api players

--- routes/player.js
+++ routes/player.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { getPlayerDetail } = require('../controllers/playerController');
+const express = require("express")
+const { getPlayerDetail, getPlayerStats, getPlayerCareerStats } = require("../controllers/playerController")
 
 const router = express.Router()
 
-router.get('/:playerId', getPlayerDetail)
+router.get("/:playerId", getPlayerDetail) // Lấy thông tin chi tiết cầu thủ
+router.get("/stats/:playerId", getPlayerStats) // Lấy thống kê cầu thủ
 
-module.exports = router;
+module.exports = router
